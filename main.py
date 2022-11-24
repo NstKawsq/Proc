fd144fcb5d07bc12bd3e60cba835afcb9eeb3b2f
NstKawsq <stfobaeq@gmail.com>
2022-11-25 00:18:54 +0700

    7laba

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import cont_construct
 import container_in
 import container_out
 import container_clean
+import multimethod
 
 
 container=cont_construct.container_construct()
 container_in.container_filling(container)
+multimethod.multik(container)
+
 container_out.print_container(container)
 container_clean.container_delete(container)
 container_out.print_container(container)
